3415cc00937f90b5e64a7d6a46f2e1cc87e0a5bf
Xiomara Figueroa <figueroaxiomara7@gmail.com>
2017-04-01 23:40:44 -0400

    Email template

--- app/app.js
+++ app/app.js
@@ -1,32 +1,107 @@
 var express = require('express'); 
 var firebase = require('firebase'); 
 var admin = require("firebase-admin");
-
-var app = express(); 
+var parser = require('body-parser')
+var stripe = require('stripe')('sk_test_4FkZmWx3xMJrOablJBUcqqlG');
 var serviceAccount = require("../Resources/serviceAccount.json");
+var nodemailer = require('nodemailer');
+var app = express(); 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://donatenow-c7865.firebaseio.com"
 });
 
-app.get('/', function(request, response) {
-	response.send("Hello World")
+app.use(parser.json());
+app.use(parser.urlencoded({
+	extended: true
+})); 
+
+app.get('/', function(req, res) {	
+	res.send('HOME'); 
 }); 
 
-// Register
-// Login
-// CreateCampaign
-// Donate
-// UpdateProfile
 // Stripe
+app.get('/donate', function(req, res) {
+	var cardID = req.body.cardID;
+	var amount = req.body.amount; 
+	var source = getSource(cardID, amount); 
 
-// app.get('/orgs', function(request, response) {
-// 	var userRef = admin.database().ref('/organization');
-//   	userRef.once('value').then(function(snapshot) {
-//     	response.send(snapshot.val().name);
-//     }); 
-// }); 
+	response.send('DONATE'); 
+}); 
 
+// Create Source 
+function getSource(doneeCard, amount) {
+	var source = stripe.sources.create({
+  	amount: amount,
+  	currency: "usd",
+  	type: "three_d_secure",
+  	three_d_secure: {card: doneeCard},
+  	redirect: {return_url: ""},
+	
+	}, function(err, source) {
+  	if(err != null) {
+  		// Check if source is ready to use
+  		// If not, try regular card type
+  		chargeCard(source, amount); 
+
+  	} else {
+  		//
+  	}
+	});
+
+	return source; 
+}
+
+function chargeCard(source, amount) {
+	stripe.charges.create({
+		amount: 2000,
+		currency: "usd",
+		source: source,
+		description: "Donation"
+
+	}, function(err, charge) {
+		if(err != null) {
+			response.send('error' + err)
+		} else {
+			response.send('charge' + charge)
+		}
+	});
+}
+
+// Email
+app.get('/email', function(req, res) {
+	var email = req.body.email; 
+	var password = req.body.password; 
+	var from = req.body.doneeEmail; 
+	var to = req.body.orgEmail; 
+	var subject = req.body.subject; 
+
+	// create reusable transporter object using the default SMTP transport
+	let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: email,
+      pass: password
+    }
+	});
+
+	// setup email data with unicode symbols
+	let mailOptions = {
+    from: from, 
+    to: to, 
+    subject: subject,
+    text: 'Hello world ?', // plain text body
+    html: '<b>Hello world ?</b>' // html body
+	};
+
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+	});
+}); 
 
 app.listen(3000); 
